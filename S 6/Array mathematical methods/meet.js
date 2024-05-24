51c1f34636e6af7e67c1fb59f222946c3c6ed1fe
Sina Rahimi <sinarahimi1382@gmail.com>
2024-05-24 14:20:04 +0330

    intial commit

--- S 6/Array mathematical methods/meet.js
+++ S 6/Array mathematical methods/meet.js
@@ -1,4 +1,9 @@
+
+// meet of two arrays
+
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [3, 4, 5, 6, 7];
-let arr3 = [...arr1, ...arr2].sort((a, b) => a - b);
-arr3.reduce((x,y)=>x-y)
+
+let meet = arr1.filter((element) => arr2.includes(element));
+
+console.log(meet);
--- S 6/Array mathematical methods/union.js
+++ S 6/Array mathematical methods/union.js
@@ -1,3 +1,5 @@
+// union of two arrays
+
 let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [3, 4, 5, 6, 7];
 let arr3 = [...arr1, ...arr2];
